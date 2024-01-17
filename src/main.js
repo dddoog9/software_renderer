6b53ae8be0c6d51db4a9981e8977fa2419491103
dddoog9 <oscar.beluchi.050@gmail.com>
2024-01-17 19:34:42 +0300

    added functions put_pixel & color32

--- src/main.js
+++ src/main.js
@@ -4,13 +4,34 @@ import {
   create_canvas,
   put_framebuffer
 } from "./renderer/canvas";
-import {create_framebuffer} from "./renderer/framebuffer";
+import {
+  create_framebuffer,
+  put_pixel
+} from "./renderer/framebuffer";
 
 const VIEWPORT_WIDTH = 800;
 const VIEWPORT_HEIGHT = 600;
 
 const canvas_cx = create_canvas(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
 const framebuffer = create_framebuffer(VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
+
+const rect_width = 100;
+const rect_height = 200;
+const rect_x = 100;
+const rect_y = 100;
+const rect_r = 1;
+const rect_g = 0;
+const rect_b = 1;
+for (let x = 0; x < rect_width; x++) {
+  for (let y = 0; y < rect_height; y++) {
+    put_pixel(
+      framebuffer,
+      rect_x + x,
+      rect_y + y,
+      rect_r, rect_g, rect_b
+    );
+  }
+}
 
 put_framebuffer(canvas_cx, framebuffer, 0, 0, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
 document.body.appendChild(canvas_cx.canvas);
--- src/renderer/framebuffer.js
+++ src/renderer/framebuffer.js
@@ -1,7 +1,10 @@
 
 export const COLOR_COMPONENTS_NUMBER = 4;
+export const MAX_COLOR8 = 255;
 
 export function create_framebuffer(w, h) {
+  w |= 0;
+  h |= 0;
   const size = w*h;
   const color_buffer = new ArrayBuffer(size*COLOR_COMPONENTS_NUMBER);
   return {
@@ -13,5 +16,20 @@ export function create_framebuffer(w, h) {
   };
 }
 
+export function color32(r, g, b) {
+  r *= MAX_COLOR8;
+  g *= MAX_COLOR8;
+  b *= MAX_COLOR8;
+  return (
+    r |
+    g << 8 |
+    b << 16 |
+    MAX_COLOR8 << 24
+  );
+}
+
 export function put_pixel(framebuffer, x, y, r, g, b) {
+  x |= 0;
+  y |= 0;
+  framebuffer.color32[ (x + y*framebuffer.width) ] = color32(r, g, b);
 }
