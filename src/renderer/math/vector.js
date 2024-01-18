efe6103a8a7da36ca1e3804c66595d89dc07b0bb
dddoog9 <oscar.beluchi.050@gmail.com>
2024-01-18 21:19:39 +0300

    added functions vector_scale, vector_scale2

--- src/renderer/math/vector.js
+++ src/renderer/math/vector.js
@@ -1,12 +1,20 @@
 
 export function vector_add(n, a, b, c) {
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < n; i++)
     c[i] = a[i] + b[i];
-  }
 }
 
 export function vector_subtract(n, a, b, c) {
-  for (let i = 0; i < n; i++) {
+  for (let i = 0; i < n; i++)
     c[i] = a[i] - b[i];
-  }
+}
+
+export function vector_scale(n, src, factor, dest) {
+  for (let i = 0; i < n; i++)
+    dest[i] = src[i]*factor;
+}
+
+export function vector_scale2(n, src, factor) {
+  for (let i = 0; i < n; i++)
+    src[i] *= factor;
 }
