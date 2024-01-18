fd51cb3ed1339bb9d4724da343cd9f18b087f383
dddoog9 <oscar.beluchi.050@gmail.com>
2024-01-18 14:54:57 +0300

    added functions vector_add, vector_subtract

--- src/renderer/math/vector.js
+++ src/renderer/math/vector.js
@@ -1,2 +1,12 @@
 
-export function add(n, a, b, c) {}
+export function vector_add(n, a, b, c) {
+  for (let i = 0; i < n; i++) {
+    c[i] = a[i] + b[i];
+  }
+}
+
+export function vector_subtract(n, a, b, c) {
+  for (let i = 0; i < n; i++) {
+    c[i] = a[i] - b[i];
+  }
+}
